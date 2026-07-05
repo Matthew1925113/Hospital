c64f0788746f44009f598c7a1976d8d1f0f293f8
matthew bermudez <bequimatt.2005@gmail.com>
2026-07-05 14:53:30 -0600

    feat: agrega controladores de inicio/login y vista login

---
+++ src/main/java/com/Proyecto/Hospital/controller/InicioController.java
@@ -0,0 +1,17 @@
+package com.Proyecto.Hospital.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioController {
+    @GetMapping("/inicio")
+    public String inicio(){
+        return "inicio";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
