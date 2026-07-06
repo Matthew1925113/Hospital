16d91e605c1f0da6b973dbc9f6b3859f89679809
matthew bermudez <bequimatt.2005@gmail.com>
2026-07-05 23:01:51 -0600

    Se agrega clases medico

--- src/main/java/com/Proyecto/Hospital/controller/MedicoController.java
+++ src/main/java/com/Proyecto/Hospital/controller/MedicoController.java
@@ -1,13 +1,42 @@
 package com.Proyecto.Hospital.controller;
-
+ 
+import com.Proyecto.Hospital.Model.Medico;
+import com.Proyecto.Hospital.Service.MedicoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+ 
 @Controller
 public class MedicoController {
 
+    @Autowired
+    private MedicoService service;
+
     @GetMapping("/medicos")
-    public String medicos() {
-        return "medicos";
+    public String index(Model model) {
+        model.addAttribute("medicos", service.ListarMedicos());
+        return "ListaMedicos";
+    }
+
+    @GetMapping("/medicoNuevo")
+    public String newMedico(Model model) {
+        model.addAttribute("medico", new Medico());
+        return "FormularioMedico";
+    }
+
+    @GetMapping("/EditarMedico/{id}")
+    public String editMedico(@PathVariable Long id, Model model) {
+        model.addAttribute("medico", service.ObtenerMedico(id));
+        return "FormularioMedico";
+    }
+
+    @PostMapping("/GuardarMedico")
+    public String updateMedico(@ModelAttribute Medico medico) {
+        service.GuardarMedico(medico);
+        return "redirect:/medicos";
     }
 }
